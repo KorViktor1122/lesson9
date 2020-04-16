2753dcf62a8693037afc2a6d104f96219295fe4b
Viktor Korneev <korneev1122@gmail.com>
2020-04-16 16:12:59 +0300

    Lesson7 path 7, first commint

---
+++ src/lesson7/HandClassMadeTest.java
@@ -0,0 +1,11 @@
+package lesson7;
+
+public class HandClassMadeTest {
+    public static void main(String[] args) {
+        Tests_Set tests_set = new Test_Set();
+        Begin(Test_Set.getClass());
+
+        System.out.println();
+        ClassCheck.outClassDate(String.class);
+    }
+}
